fe2f9057b46b5657030dad5250643acfbd8087a5
jhinojosa-nearsoft <jhinojosa@nearsoft.com>
2013-09-30 10:00:38 -0700

    Initial Commit

---
+++ DesignPatternsTest/SingletonTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesignPatterns;
+using System.IO;
+using DesignPatterns.DesignPatternsEnum;
+
+namespace DesignPatternsTest
+{
+    [TestClass]
+    public class SingletonTests
+    {
+        [TestMethod]
+        public void SingletonCanonical()
+        {
+            String singleton = SingletonBuilder.BuildSingleton("MySingletonCanonical", SingletonType.Canonical);
+
+            FileWriter fw = new FileWriter(@"C:\Users\JuanLorenzo\Desktop\MySingletonCanonical.cs", singleton);
+            fw.WriteFile();
+
+            Assert.IsTrue(File.Exists(@"C:\Users\JuanLorenzo\Desktop\SingletonCanonical.cs"));
+        }
+        [TestMethod]
+        public void SingletonStaticInitialization()
+        {
+            String singleton = SingletonBuilder.BuildSingleton("MySingletonStaticInitialization", SingletonType.StaticInitialization);
+
+            FileWriter fw = new FileWriter(@"C:\Users\JuanLorenzo\Desktop\MySingletonStaticInitialization.cs", singleton);
+            fw.WriteFile();
+
+            Assert.IsTrue(File.Exists(@"C:\Users\JuanLorenzo\Desktop\test.cs"));
+        }
+
+        [TestMethod]
+        public void SingletonCanonicalXML()
+        {
+            String singleton = SingletonBuilder.BuildSingletonFromXml("MySingletonStaticInitialization", "SingletonCanonical");
+
+            FileWriter fw = new FileWriter(@"C:\Users\JuanLorenzo\Desktop\MySingletonStaticInitialization.cs", singleton);
+            fw.WriteFile();
+
+            Assert.IsTrue(File.Exists(@"C:\Users\JuanLorenzo\Desktop\test.cs"));
+        }
+    }
+}
---
+++ DesignPatternsTest/test.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class MySingleton
+{
+    private static MySingleton instance;
+
+    private MySingleton() {}
+
+    public static MySingleton Instance
+    {
+        get 
+        {
+            if (instance == null)
+            {
+                instance = new MySingleton();
+            }
+            return instance;
+        }
+    }
+}
